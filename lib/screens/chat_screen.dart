44266a12f0174dcf330a40a087b6ea82a90b4bee
Hyun Lee <wildriver1105@gmail.com>
2025-10-03 22:49:25 +0900

    New sailboats added

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -19,6 +19,40 @@ class ChatScreen extends StatelessWidget {
         return 'Farr40';
       case 'benetau473':
         return 'Benetaur473';
+      case 'j24':
+        return 'J/24';
+      case 'laser':
+        return 'Laser';
+      case 'swan50':
+        return 'Swan 50';
+      case 'x35':
+        return 'X-35';
+      case 'melges32':
+        return 'Melges 32';
+      case 'tp52':
+        return 'TP52';
+      case 'first36':
+        return 'Beneteau First 36';
+      case 'sunfast3300':
+        return 'Jeanneau Sun Fast 3300';
+      case 'dehler38':
+        return 'Dehler 38';
+      case 'xp44':
+        return 'X-Yachts XP 44';
+      case 'hanse458':
+        return 'Hanse 458';
+      case 'oceanis46':
+        return 'Beneteau Oceanis 46';
+      case 'swan48':
+        return 'Nautor Swan 48';
+      case 'gc42':
+        return 'Grand Soleil GC 42';
+      case 'rs21':
+        return 'RS21';
+      case 'j70':
+        return 'J/70';
+      case 'solaris44':
+        return 'Solaris 44';
       default:
         return modelId;
     }
