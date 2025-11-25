b667b6725d63a008ad5c3b6cca4c293cc2f8ae4d
Hyun Lee <wildriver1105@gmail.com>
2025-11-25 22:52:59 +0900

    Firebase added to the main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,15 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'screens/main_screen.dart';
 import 'constants/colors.dart';
 import 'screens/dummy_main.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   try {
     await dotenv.load(fileName: ".env");
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
   } catch (e) {
     print('Warning: Could not load .env file: $e');
   }
